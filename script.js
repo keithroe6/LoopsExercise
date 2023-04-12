f579e6f38c8681cab07cd8883f2f638ee42dcb16
Keith Roe <kdroe1@gmail.com>
2023-04-12 11:15:09 -0400

    After review in class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. 
+// 1.  
 for (i=1; i<8; i++){
     console.log(i);
 }
@@ -12,9 +12,12 @@ for (i=5; i < 26; i=i+4) {
 const wizards = [`Harry Potter`, `Hermione Granger`, `Ron Weasley`];
 
 // 3b.
-for (i=0; i<3; i++) {
-    console.log(wizards[i]);
-}
+// for (i=0; i<3; i++) {         Did not use a For Of loop
+//     console.log(wizards[i]);
+// }
+for (wiz of wizards) {
+    console.log(wiz);
+};
 
 // 4a. 
 let harryPotterMovies = 0;
@@ -22,7 +25,9 @@ let harryPotterMovies = 0;
 // 4b. 
 while (harryPotterMovies < 8) {
     harryPotterMovies++;
-}
+};
+
+// 4c.
 console.log(harryPotterMovies);
 
 // BONUS
@@ -31,20 +36,26 @@ console.log(harryPotterMovies);
 const hogwartsHouses = [`Gryffindor`, `Hufflepuff`, `Ravenclaw`, `Slytherin`];
 
 // 5b.
-for(i=0; i<4; i++ ){
- if (i<2) {
-    for(n=0; n<=9; n++){
-console.log(hogwartsHouses[i][n]);
-}}
- else {
-    for(n=0; n<=8; n++){
-        console.log(hogwartsHouses[i][n]);
-        }
-}
+// for(i=0; i<4; i++ ){      once again did not use For Of loops
+//  if (i<2) {
+//     for(n=0; n<=9; n++){
+// console.log(hogwartsHouses[i][n]);
+// }}
+//  else {
+//     for(n=0; n<=8; n++){
+//         console.log(hogwartsHouses[i][n]);
+//         }
+// }
+// }
+
+for (house of hogwartsHouses) {
+    for(i of house){
+        console.log(i);
+    }
 }
 
 // 6a.
-const quote = [`Yer`,`A`,`Wizard`,`Harry`];
+const quote = [`Yer`,`A`,`Wizard`,`Harry`]; 
 
 // 6b.
 let completeQuote = ` `;
@@ -55,7 +66,7 @@ for (i=0; i< 4; i++) {
 }
 console.log(completeQuote);
 
-// 7.
+// 7. Inspired by FizzBuzz
 for (i=1; i<26; i++) {
     let div3 = i%3;
     let div5 = i%5;
